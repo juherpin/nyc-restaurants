d54c063200df4e1d4b67c98eda3cbad3d36ab0ec
Darry <darry.jdp@gmail.com>
2016-04-12 10:01:41 +0200

    change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
-
+var Restaurant = require('../models/restaurants');
+// var comment = require('')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
+router.get('/restaurants', function(req, res, next) {
+  Restaurant.aggregate(
+      [
+        { $unwind : "$grades.score " },
+          // { "$group": {
+          //     "_id": {
+          //       name: '$name',
+          //       cuisine: '$cuisine',
+          //       id: '$restaurant_id'
+          //     },
+          //     "score": { "$avg":  "$grades.score " }
+          //   }
+          // },
+          // { "$sort": { "score": -1 } },
+          // { "$limit": 10 }
+      ],
+      function(err,result) {
+        if(err){
+          res.send(err)
+        } else {
+          res.send(result)
+          // res.render('restaurants/index', {restaurants:result , title:'goodMeal'});
+        }
+      }
+  );
+  //
+});
+router.get('/restaurant/:id', function(req, res, next) {
+  Restaurant.aggregate(
+      [
+          { "$group": {
+              "_id": {
+                name: '$name',
+                cuisine: '$cuisine',
+                id: '$restaurant_id'
+              },
+              "score": { "$avg":  '$grades.score'  }
+            }
+          },
+          { "$sort": { "score": -1 } },
+          { "$limit": 10 }
+      ],
+      function(err,result) {
+        if(err){
+          res.send(err)
+        } else {
+          res.send(result)
+          // res.render('restaurants/index', {restaurants:result , title:'goodMeal'});
+        }
+      }
+  );
+  //
+});
 module.exports = router;
