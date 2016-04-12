39854dd7def7916494482062b07260075d472151
Darry <darry.jdp@gmail.com>
2016-04-12 11:49:08 +0200

    add handler comments

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var Restaurant = require('../models/restaurants');
-// var comment = require('')
+var Comment = require('../models/comments');
+
+var getLastComment(){
+  Comment
+  .find({})
+  .populate('restaurant')
+  .exec(function (err, comments) {
+    if (err){
+      res.json(err)
+    }else{
+      res.json(comments)
+    }
+  });
+}
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 
-
+router.post('/comment', (req, res, next )=>{
+  var comment = new Comment({
+    restaurant: req.body.restaurant,
+    user: req.body.user,
+    title: req.body.title,
+    comment:req.body.comment
+  })
+    comment.save(function (err) {
+    if (err){
+      res.render('error',{error: err})
+    }
+    res.redirect(`/restaurant/${req.body.restaurant}`);
+  })
+});
 router.get('/restaurants', function(req, res, next) {
   Restaurant.aggregate(
       [
