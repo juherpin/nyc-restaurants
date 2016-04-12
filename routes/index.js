eac4e631e77dda80e57ec1ac85dffb721da7e9fa
Darry <darry.jdp@gmail.com>
2016-04-12 10:36:27 +0200

    add retaurants/:id route

--- routes/index.js
+++ routes/index.js
@@ -7,57 +7,30 @@ var Restaurant = require('../models/restaurants');
 //   res.render('index', { title: 'Express' });
 // });
 
+
 router.get('/restaurants', function(req, res, next) {
   Restaurant.aggregate(
       [
-        { $unwind : "$grades.score " },
-          // { "$group": {
-          //     "_id": {
-          //       name: '$name',
-          //       cuisine: '$cuisine',
-          //       id: '$restaurant_id'
-          //     },
-          //     "score": { "$avg":  "$grades.score " }
-          //   }
-          // },
-          // { "$sort": { "score": -1 } },
-          // { "$limit": 10 }
-      ],
-      function(err,result) {
-        if(err){
-          res.send(err)
-        } else {
-          res.send(result)
-          // res.render('restaurants/index', {restaurants:result , title:'goodMeal'});
-        }
-      }
-  );
-  //
-});
-router.get('/restaurant/:id', function(req, res, next) {
-  Restaurant.aggregate(
-      [
-          { "$group": {
-              "_id": {
-                name: '$name',
-                cuisine: '$cuisine',
-                id: '$restaurant_id'
-              },
-              "score": { "$avg":  '$grades.score'  }
-            }
-          },
-          { "$sort": { "score": -1 } },
+          { $unwind : '$grades' },
+          { "$sort": { "grades.score": -1 } },
           { "$limit": 10 }
       ],
       function(err,result) {
         if(err){
           res.send(err)
         } else {
-          res.send(result)
-          // res.render('restaurants/index', {restaurants:result , title:'goodMeal'});
+          //res.send(result)
+          res.render('restaurants/index', {restaurants:result , title:'goodMeal'});
         }
       }
   );
+  //
+});
+router.get('/restaurant/:id', function(req, res, next) {
+  Restaurant.findOne(
+    {restaurant_id: req.params.id}, function(err,result) {
+
+      });
   //
 });
 module.exports = router;
